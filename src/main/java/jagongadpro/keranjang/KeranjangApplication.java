90188fe3ba0914924ce7f4b4deb47a28b1c34e93
suryata <madeputra415@gmail.com>
2024-04-17 11:48:01 +0700

    fixing docker

--- src/main/java/jagongadpro/keranjang/KeranjangApplication.java
+++ src/main/java/jagongadpro/keranjang/KeranjangApplication.java
@@ -2,8 +2,9 @@ package jagongadpro.keranjang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KeranjangApplication {
 
 	public static void main(String[] args) {
