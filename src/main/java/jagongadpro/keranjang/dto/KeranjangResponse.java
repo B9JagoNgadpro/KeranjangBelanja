47674e15235d7e650d03dfcf3547bd195b20aa39
suryata <madeputra415@gmail.com>
2024-05-01 18:12:48 +0700

    [REFACTOR] Refactoring the DTO using lombok getter and setter

--- src/main/java/jagongadpro/keranjang/dto/KeranjangResponse.java
+++ src/main/java/jagongadpro/keranjang/dto/KeranjangResponse.java
@@ -1,40 +1,16 @@
 package jagongadpro.keranjang.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
 import java.util.Map;
 
+@Getter
+@Setter
+@AllArgsConstructor 
 public class KeranjangResponse {
     private String email;
     private Map<Integer, Integer> items;
     private double totalPrice;
-
-    public KeranjangResponse(String email, Map<Integer, Integer> items, double totalPrice) {
-        this.email = email;
-        this.items = items;
-        this.totalPrice = totalPrice;
-    }
-
-    // Getter dan Setter
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Map<Integer, Integer> getItems() {
-        return items;
-    }
-
-    public void setItems(Map<Integer, Integer> items) {
-        this.items = items;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
 }
--- src/main/java/jagongadpro/keranjang/dto/WebResponse.java
+++ src/main/java/jagongadpro/keranjang/dto/WebResponse.java
@@ -1,40 +1,16 @@
 package jagongadpro.keranjang.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor // Anotasi untuk menghasilkan constructor tanpa argumen
+@AllArgsConstructor // Anotasi untuk menghasilkan constructor dengan semua argumen
 public class WebResponse<T> {
     private T data; // Data yang akan dikirimkan sebagai respons
     private String message; // Pesan yang dapat digunakan untuk menyampaikan informasi tambahan atau kesalahan
     private boolean success; // Status untuk menunjukkan apakah operasi berhasil atau tidak
-
-    public WebResponse() {}
-
-    public WebResponse(T data, String message, boolean success) {
-        this.data = data;
-        this.message = message;
-        this.success = success;
-    }
-
-    // Getter dan Setter
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
 }
