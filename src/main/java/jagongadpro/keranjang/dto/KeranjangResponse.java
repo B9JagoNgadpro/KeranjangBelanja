b34597962b7c8b4327a07f71fc3b7ce0e1a9f4cb
suryata <madeputra415@gmail.com>
2024-05-14 13:33:09 +0700

    [GREEN] Implementing GameDto for high level networking

--- src/main/java/jagongadpro/keranjang/dto/KeranjangResponse.java
+++ src/main/java/jagongadpro/keranjang/dto/KeranjangResponse.java
@@ -11,6 +11,8 @@ import java.util.Map;
 @AllArgsConstructor 
 public class KeranjangResponse {
     private String email;
+
     private Map<String, Integer> items;
+    
     private double totalPrice;
 }
