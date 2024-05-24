09c87184849e94299b398fe73e35e002a7d82292
suryata <madeputra415@gmail.com>
2024-05-24 17:10:10 +0700

    [REFACTOR] Try to get coverage

--- src/test/java/jagongadpro/keranjang/KeranjangApplicationTests.java
+++ src/test/java/jagongadpro/keranjang/KeranjangApplicationTests.java
@@ -1,6 +1,6 @@
 package jagongadpro.keranjang;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest()
