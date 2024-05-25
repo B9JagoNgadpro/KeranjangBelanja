3bc82ad8ef0f789d198fc011b7154e6c370abf4a
suryata <madeputra415@gmail.com>
2024-05-25 17:44:14 +0700

    [FIX] Fixing foreign key

--- src/main/java/jagongadpro/keranjang/model/ShoppingCart.java
+++ src/main/java/jagongadpro/keranjang/model/ShoppingCart.java
@@ -17,7 +17,7 @@ public class ShoppingCart {
     private String email;
 
     @ElementCollection
-    @CollectionTable(name = "cart_items", joinColumns = @JoinColumn(name = "email"))
+    @CollectionTable(name = "cart_items")
     @MapKeyColumn(name = "item_id")
     @Column(name = "quantity")
     private Map<String, Integer> items = new HashMap<>();
