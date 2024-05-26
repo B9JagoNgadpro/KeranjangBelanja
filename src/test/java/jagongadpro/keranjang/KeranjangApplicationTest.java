3a137d625142dc435bcdf6ef82cce93347c9ce93
suryata <madeputra415@gmail.com>
2024-05-26 20:13:52 +0700

    [FIX] Trying to test

--- src/test/java/jagongadpro/keranjang/KeranjangApplicationTest.java
+++ src/test/java/jagongadpro/keranjang/KeranjangApplicationTest.java
@@ -1,13 +1,21 @@
 package jagongadpro.keranjang;
 
+import jagongadpro.keranjang.config.GameApiProperties;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootTest
-class KeranjangApplicationTests {
+@EnableScheduling
+@EnableAsync
+@EnableConfigurationProperties(GameApiProperties.class)
+public class KeranjangApplicationTest {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
 
     }
 }
